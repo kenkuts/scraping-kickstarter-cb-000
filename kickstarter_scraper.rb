fabe9ffd2c9859392d097c3bcd79891f5636e4b0
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-06-12 23:50:30 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,8 +2,8 @@ require 'nokogiri'
 require 'pry'
 
 def create_project_hash
-  html = File.read("fixtures/kickstarter.html")
-  kickstarter = Nokogiri::HTML(html)
+  html = 
+  kickstarter = Nokogiri::HTML(File.read("fixtures/kickstarter.html"))
   post_hash = {}
   kickstarter.css("li.project.grid_4").each do |post|
     title = post.css("div h2.bbcard_name strong a").text
@@ -12,7 +12,7 @@ def create_project_hash
       :image_link => post.css("div.project-thumbnail a img").attribute("src").value,
       :description => post.css("div p.bbcard_blurb").text,
       :location => post.css("ul.project-meta span.location-name").text,
-      :percent_funded => post.css("ul.project-status li.pleged strong").text, #.gsub("%","").to_i
+      :percent_funded => post.css("ul.project-status li.pleged strong").text #.gsub("%","").to_i
     }
  end
 
