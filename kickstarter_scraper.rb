0df5c73d21a16da58765431712cabe751d04090c
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-06-12 23:37:04 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,12 +7,20 @@ def create_project_hash
   kickstarter.css("li.project.grid_4").each do |post|
     title = post.css("div h2.bbcard_name strong a").text
     
-    post_hash[title.to_sym] = {
-      :image_link => post.css("div.project-thumbnail a img").attribute("src").value,
-      :description => post.css("div p.bbcard_blurb").text,
-      :location => post.css("ul.project-meta span.location-name").text,
-      :percent_funded => post.css("ul.project-status li.pleged strong").text,
-    }
+    # post_hash[title.to_sym] = {
+    #   :image_link => post.css("div.project-thumbnail a img").attribute("src").value,
+    #   :description => post.css("div p.bbcard_blurb").text,
+    #   :location => post.css("ul.project-meta span.location-name").text,
+    #   :percent_funded => post.css("ul.project-status li.pleged strong").text,
+    # }
+    
+   title = project.css("h2.bbcard_name strong a").text
+   projects[title.to_sym] = {
+     :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+     :description => project.css("p.bbcard_blurb").text,
+     :location => project.css("ul.project-meta span.location-name").text,
+     :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+   }
 
   end
 
